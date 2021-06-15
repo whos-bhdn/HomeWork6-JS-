38a080dca1fa953b2992e9abe4bf56f4dbaba15a
whos-bhdn <anonimoos13@gmail.com>
2021-06-15 11:54:10 +0300

    not complete

---
+++ task3.js
@@ -0,0 +1,35 @@
+// Завдання 3
+
+// Написати розв’язок нижче описаного завдання за допомогою function expression or arrow:
+// 1.	Створіть функцію, в яку передається 2 параметри, 
+// які користувач вводить через вікно prompt(). 
+// 2.	Потрібно щоб функція виводила діапазон тільки простих чисел
+//  console.log(), між тими які ввів користувач.
+// Приклад роботи:
+// simpleNumber(10,15) – має повернути 11, 13
+// simpleNumber (2,20) – має повернути 2,3,5,7,11,13,17,19
+
+// min = prompt(parseInt('Enter start number'));
+// max = prompt(parseInt('Enter end number'))
+
+// const simpleNumber = function(min,max) {
+//    for (let i = 2; i <= n; i++) {
+//               if (n % i === 0) {
+//                   return false
+//               }
+//           }
+//           return console.log(n)
+// console.log(simpleNumber())
+
+
+
+// Класна робота
+// function isSimple(n) {
+   //     for (let i = 2; i <= n; i++) {
+   //         if (n % i === 0) {
+   //             return false
+   //         }
+   //     }
+   //     return true
+   // }
+   // isSimple(4)
